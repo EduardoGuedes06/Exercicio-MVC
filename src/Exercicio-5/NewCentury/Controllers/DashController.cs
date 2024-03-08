862c56cbc0d3153bb4f72490829a8111d819f062
Eduardo Guedes Pereira Silva <eduardoguedespsn@gmail.com>
2024-03-08 13:19:04 -0300

    Pagina de Fim de jogo

---
+++ src/Exercicio-5/NewCentury/Controllers/DashController.cs
@@ -0,0 +1,57 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using NewCentury.Data.Repository;
+using NewCentury.Domain.Intefaces;
+using NewCentury.Domain.Models;
+using NewCentury.Service.Services;
+using NewCentury.ViewModels;
+using NewCentury.ViewModels.Temp;
+
+namespace NewCentury.Controllers
+{
+    public class DashController : Controller
+    {
+        private readonly IMapper _mapper;
+        private readonly IGameService _gameService;
+        private readonly IJogadorRepository _jogadorRepository;
+        private readonly IJogadorService _jogadorService;
+        private readonly IHistoricoTentativaRepository _historicoTentativaoRepository;
+        private readonly IHistoricoTentativaService _historicoTentativaService;
+        private readonly IPartidaService _partidaService;
+        private readonly IPartidaRepository _partidaRepository;
+        private readonly IRodadaRepository _rodadaRepository;
+        private readonly IRodadaService _rodadaService;
+
+        public DashController(IGameService gameService,
+                                 IPartidaRepository partidaRepository,
+                                 IRodadaRepository rodadaRepository,
+                                 IMapper mapper, IJogadorRepository jogadorRepository,
+                                 IPartidaService partidaService,
+                                 IRodadaService rodadaService,
+                                 IJogadorService jogadorService,
+                                 IHistoricoTentativaRepository historicoTentativaoRepository,
+                                 IHistoricoTentativaService historicoTentativaService,
+                                 INotificador notificador)
+        {
+            _rodadaRepository = rodadaRepository;
+            _mapper = mapper;
+            _partidaService = partidaService;
+            _partidaRepository = partidaRepository;
+            _rodadaService = rodadaService;
+            _gameService = gameService;
+            _jogadorService = jogadorService;
+            _jogadorRepository = jogadorRepository;
+            _historicoTentativaoRepository = historicoTentativaoRepository;
+            _historicoTentativaService = historicoTentativaService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+
+
+    }
+}
