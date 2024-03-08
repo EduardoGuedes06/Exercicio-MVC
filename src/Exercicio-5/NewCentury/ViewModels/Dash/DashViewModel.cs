e6a8e42b338e187022ff50cd48b3adb9e2a0a858
Eduardo Guedes Pereira Silva <eduardoguedespsn@gmail.com>
2024-03-08 16:43:05 -0300

    X

--- src/Exercicio-5/NewCentury/ViewModels/Dash/DashViewModel.cs
+++ src/Exercicio-5/NewCentury/ViewModels/Dash/DashViewModel.cs
@@ -4,7 +4,10 @@
     {
         public DateTime DataInicial { get; set; }
         public DateTime DataFinal { get; set; }
-        public string NomeJogador { get; set; }
+        public string FiltroHistoricoNomeJogador { get; set; }
+        public string FiltroRankingNomeJogador { get; set; }
+
+        public string DificuldadePartida { get; set; }
 
         public Guid IdJogador { get; set; }
         public Guid IdPartida { get; set; }
